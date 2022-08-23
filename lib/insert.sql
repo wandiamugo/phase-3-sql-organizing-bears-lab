9ef12d4ec8bdf7ab3c384dc2007f91f520eab68d
wandiamugo <wwandia173@gmail.com>
2022-08-23 23:41:43 +0300

    commit tree 701ccc9d6e264e3b76552d713f4de62ca57b7f60

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name ,sex) VALUES('Mr.Chocolate' ,'female');
+INSERT INTO bears (name ,sex) VALUES('Rowdy' ,'male');
+INSERT INTO bears (name ,sex) VALUES('Tabitha' ,'female');
+INSERT INTO bears (name ,sex) VALUES('Sargeant Brown' ,'male');
+INSERT INTO bears (name ,sex) VALUES('Melissa' ,'female');
+INSERT INTO bears (name ,sex) VALUES('Grinch', 'male');
+INSERT INTO bears (name ,sex) VALUES('Wendy' ,'female');
+INSERT INTO bears (name ,sex) VALUES('NULL' ,'female');
