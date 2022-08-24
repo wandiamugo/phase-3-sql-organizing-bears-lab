f88e6928daabdaec046235aadb8d4739bb082590
Elizabeth Wandia Mugo <wwandia173@gmail.com>
2022-08-24 16:31:55 +0300

    Done

---
+++ lib/create.sql
@@ -0,0 +1,1 @@
+CREATE TABLE Bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, sex TEXT, color TEXT, temperament TEXT, alive BOOLEAN);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate' ,4, 'M', 'brown', 'suportive', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy' ,4, 'M', 'brown', 'optimistic', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha' ,4, 'F', 'brown', 'loving', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown' ,4, 'M', 'dark', 'bossy', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa' ,4, 'F', 'dark', 'suportive', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch' ,4, 'M', 'brown', 'pessimistic', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy' ,4, 'F', 'brown', 'choleric', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL ,4, 'M', 'dark', 'phalgmatic', TRUE);
